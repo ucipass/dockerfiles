f35664e4ab415175d716c2da534a69025132462e
ucipass@gmail.com <ucipass@gmail.com>
2020-09-22 09:25:05 -0500

    openconnect

--- openconnect/openconnect.sh
+++ openconnect/openconnect.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 pidfile="/tmp/openconnect-pid"
+CMD="openconnect --pid-file=$pidfile --csd-user=root --csd-wrapper=/root/.cisco/csd-wrapper.sh"
 
 case "$1" in
 start)
     if [ -f $pidfile ]; then
         echo "Openconnect already started. Use $0 <start|stop>"
         exit 1
-    elif [ -n "$HOST" ] && [ -n "$USER" ] && [ -n "$PASS" ]
-        then
-        echo "ENV:HOST,USER,PASS"
-        echo $PASS | openconnect --pid-file=$pidfile --csd-user=root --csd-wrapper=/root/.cisco/csd-wrapper.sh --user=$USER -b $HOST
-
-    elif [ -n "$HOST" ] && [ -n "$USER" ]
-        then
-        echo "ENV:HOST,USER"
-        openconnect --pid-file=$pidfile --csd-user=root --csd-wrapper=/root/.cisco/csd-wrapper.sh -b $HOST --user=$USER
-    elif [ -n "$HOST" ]
-        then
-        echo "ENV:HOST"
-        openconnect --pid-file=$pidfile --csd-user=root --csd-wrapper=/root/.cisco/csd-wrapper.sh -b $HOST
-    else
-       echo "NO HOST ENVIRONMENT VARIABLE SET! Aborting...."
     fi
+
+    if [ -z "$HOST" ]; then
+        echo "NO HOST ENVIRONMENT VARIABLE SET! Aborting...."
+        exit 1
+    fi
+
+    if [ -n "$USER" ]; then
+        CMD="$CMD --user=$USER"
+    fi
+
+    if [ -n "$GROUP" ]; then
+        echo $CMD
+    fi
+
+    $CMD -b $HOST
+
     ;;
 stop)
     cat $pidfile  | xargs kill -2
