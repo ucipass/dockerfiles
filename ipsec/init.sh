b1ea4d833074e29d77e7e64c5c73ad8cb2cd434e
ucipass@gmail.com <ucipass@gmail.com>
2020-09-25 20:53:06 -0500

    ipsec

--- ipsec/init.sh
+++ ipsec/init.sh
@@ -11,57 +11,18 @@
 # - ESP
 # - ESP_LIFETIME
 
-if [ -e /etc/ipsec.init ]
-then
-    echo "Already Initialized!"
-    exit 0
-fi
+python3 ipsec.py
 
-echo "Initializing..."
-touch /etc/ipsec.init
-
-if [ ! -f /sys/class/net/loopvpn ]
+if [ -f /sys/class/net/loopvpn ]
 then
+  echo "Deleting loopvpn loopback interface..."
+  ip link delete name loopvpn
+else
   echo "Creating loopvpn loopback interface..."
   ip link add name loopvpn  type dummy
   ip link set loopvpn up
-  ip address add ${PRIV_LOCAL_IP} dev loopvpn
+  ip address add ${PRIV_LOCAL_IP} dev loopvpn  
 fi
 
-cat > /etc/ipsec.secrets <<_EOF_
-${VPN_GATEWAY} : PSK ${VPN_SECRET}
-_EOF_
-
-cat > /etc/ipsec.conf <<_EOF_
-config setup
-  charondebug="cfg 2, dmn 2, ike 2, net 2"
-  uniqueids=yes
-  strictcrlpolicy=no
-
-conn VPN_CONNECTION
-  authby=secret
-  left=%defaultroute
-  #leftid=169.254.100.1
-  leftsubnet=${PRIV_LOCAL_NET}
-  right=${VPN_GATEWAY}
-  rightsubnet=${PRIV_REMOTE_NET}
-  ike=${IKE}
-  ikelifetime=${IKE_LIFETIME}
-  esp=${ESP}
-  lifetime=${ESP_LIFETIME}
-  keyingtries=0
-  keyexchange=ikev1
-  dpddelay=30
-  dpdtimeout=120
-  dpdaction=restart
-  auto=start
-
-_EOF_
-
-if [ ! -z ${PRIV_REMOTE_NET2} ]; then
-  cat >> /etc/ipsec.conf <<_EOF_
-conn VPN_CONNECTION-1
-  also=VPN_CONNECTION
-  rightsubnet=${PRIV_REMOTE_NET2}
-_EOF_
-fi
+echo "IPSec is starting..."
+exec ipsec start --nofork "$@"
---
+++ ipsec/ipsec.py
@@ -0,0 +1,79 @@
+import ipaddress
+import os
+import sys
+import urllib.request
+
+params = {}
+params["VPN_GATEWAY"]     = ipaddress.ip_address( os.environ.get('VPN_GATEWAY')     )
+params["PRIV_REMOTE_NET"] = ipaddress.ip_network( os.environ.get('PRIV_REMOTE_NET') )
+params["PRIV_LOCAL_NET"]  = ipaddress.ip_network( os.environ.get('PRIV_LOCAL_NET')  )
+params["PRIV_LOCAL_IP"]   = ipaddress.ip_network( os.environ.get('PRIV_LOCAL_IP') ,False )
+params["VPN_SECRET"]      = os.environ.get('VPN_SECRET') 
+params["IKE"]             = os.environ.get('IKE')
+params["IKE_LIFETIME"]    = os.environ.get('IKE_LIFETIME')
+params["ESP"]             = os.environ.get('ESP')
+params["ESP_LIFETIME"]    = os.environ.get('ESP_LIFETIME')
+
+exitcode = 0
+for key in params:
+    if not params[key]: 
+        print("Environment variable missing or invalid: {}".format(key))
+        exitcode = 1
+
+try:
+    params["PUB_LOCAL_IP"] = urllib.request.urlopen('https://ifconfig.me').read().decode('utf8')
+except Exception as err:
+    print("Unable to determine public IP by going to ifconfig.me")
+    exitcode = 1
+
+if exitcode: 
+    sys.exit()
+
+
+config_dir="."
+if os.environ.get('CONFIG_DIR'): 
+    config_dir = os.environ.get('CONFIG_DIR')
+
+ipsec_config = """\
+config setup
+  charondebug="cfg 2, dmn 2, ike 2, net 2"
+  uniqueids=yes
+  strictcrlpolicy=no
+
+conn VPN_CONNECTION
+  authby=secret
+  left=%defaultroute
+  leftid={PUB_LOCAL_IP}
+  leftsubnet={PRIV_LOCAL_NET}
+  right={VPN_GATEWAY}
+  rightsubnet={PRIV_REMOTE_NET}
+  ike={IKE}
+  ikelifetime={IKE_LIFETIME}
+  esp={ESP}
+  lifetime={ESP_LIFETIME}
+  keyingtries=0
+  keyexchange=ikev1
+  dpddelay=30
+  dpdtimeout=120
+  dpdaction=restart
+  auto=start
+""".format(
+    PUB_LOCAL_IP    = params["PUB_LOCAL_IP"],
+    PRIV_LOCAL_NET  = params["PRIV_LOCAL_NET"],
+    VPN_GATEWAY     = params["VPN_GATEWAY"],
+    PRIV_REMOTE_NET = params["PRIV_REMOTE_NET"],
+    IKE             = params["IKE"],
+    IKE_LIFETIME    = params["IKE_LIFETIME"],
+    ESP             = params["ESP"],
+    ESP_LIFETIME    = params["ESP_LIFETIME"],
+)
+
+ipsec_secret = """\
+ : PSK "{}"
+""".format( params["VPN_SECRET"] )
+
+with open( os.path.join( config_dir, "ipsec.conf"), "w") as file:
+    file.write(ipsec_config)
+
+with open( os.path.join( config_dir, "ipsec.secret"), "w") as file:
+    file.write(ipsec_secret)
